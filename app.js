98f57dcdd0ed7bff0e8f2bf3fcd22fca6e84b05b
Adarsh Kumar <adars99@gmail.com>
2015-09-15 16:40:12 -0700

    NodeJs along with MongoDB and IISNode routes
    NodeJs along with MongoDB and IISNode routes. Thanks to
    http://www.dotnetcurry.com/nodejs/1032/nodejs-apps-in-visual-studio-mean-stack

---
+++ app.js
@@ -0,0 +1,34 @@
+﻿var http = require('http');
+var express = require('express');
+var routes = require('./routes')
+var bodyParser = require('body-parser');
+var path = require('path');
+var port = process.env.port || 4000;
+
+var app = express();
+app.use(bodyParser());
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/AdarshNodejs', routes.index);
+app.get('/AdarshNodejs/about', routes.about);
+app.get('/AdarshNodejs/contact', routes.contact);
+
+
+/*app.get('/AdarshNodejs', function (request, response) {
+    response.sendfile("views/HtmlPage.html");
+});*/
+
+app.use('/AdarshNodejs', express.static(path.join(__dirname, 'public')));
+
+app.listen(port);
